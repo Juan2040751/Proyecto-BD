1e749262d057ce5fb80bc8d72fa7d4bbd4485ee2
Juan2040751 <82058717+Juan2040751@users.noreply.github.com>
2022-07-10 11:33:36 -0500

    carpeta añadida

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+// eslint-disable-next-line
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// eslint-disable-next-line
+import Home from './pages/home';
+// eslint-disable-next-line
+import Registro from './pages/reg'
+
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/registro" element={<Registro/>}/>    
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+// eslint-disable-next-line
+import Home from './pages/home';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App/>
+);
+
+
