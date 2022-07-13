6d6cece74aed4aad560c745667e020430cb27048
Sheilly Ortega 2040051-3743 <sheilly.ortega@correounivalle.edu.co>
2022-07-12 22:00:44 -0500

    Created the react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './pages/home';
+import Registro from './pages/reg';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Router>
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/registro" element={<Registro/>}/>    
+              </Routes>
+          </Router>
+      </div>
   );
 }
 
---
+++ src/pages/reg.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import img from '../fondoMande';
+import './reg.css';
+
+const reg = () => {
+  return (
+    <div className='Registro'>
+      <div className='RegistroConteiner'>
+
+        <div className='containerReg'>
+          <h1>Registro</h1>
+          <form>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="name">Nombre Completo</label>
+                <input type="text" className="form-control" placeholder="Name, ej: Juan Lozano" required />
+              </div>
+            </div>
+
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="age">Edad</label>
+                <input type="number" className="form-control" id="inputAge" placeholder="Edad, ej: 17" required />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="id">Numero de Cedula</label>
+                <input type='number' className="form-control" id="inputId" placeholder="Cedula, ej: 1234567" required />
+              </div>
+
+            </div>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="barrio">Barrio</label>
+                <input type="text" className="form-control" id="inputBarrio" placeholder="Barrio, ej: Lido" required />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="calle">Calle</label>
+                <input type="text" className="form-control" id="inputCalle" placeholder="Calle, ej: 5ta" required />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="carrera">Carrera</label>
+                <input type="text" className="form-control" id="inputCarrera" placeholder="Carrera, ej: 15" required />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="cuidad">Cuidad</label>
+                <input type="text" className="form-control" id="inputCuidad" placeholder="Calle, ej: Cali" required />
+              </div>
+
+            </div>
+
+            <div className="continuarReg">
+              Escoge tu rol
+              <div className="btnReg">
+                <button type="submit" className="btnRegsSG">Trabajador</button>
+                <button type="submit" className="btnRegsLG">Usuario</button>
+              </div>
+            </div>
+
+          </form >
+        </div >
+        <div className='imgRegConteiner'>
+          <img src={img} alt='img' className='imgreg' />
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default reg
